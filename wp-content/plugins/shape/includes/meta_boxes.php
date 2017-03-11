1dfdf750dc75c5d45cfca2fc4c3da7f727ecc042
Tova Moskowitz <tovamosk@gmail.com>
2017-03-11 09:41:02 -0500

    change theme name

---
+++ wp-content/plugins/shape/includes/meta_boxes.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tova
+ * Date: 2/25/17
+ * Time: 1:00 PM
+ */
+
+
+use Carbon_Fields\Container;
+use Carbon_Fields\Field;
+
+Container::make('post_meta', 'Custom Data')->show_on_post_type('client')->set_priority('high')
+    ->add_fields(array(
+    Field::make('text', 'client_fname', 'Client First Name'),
+    Field::make('text', 'client_lname', 'Client Last Name')
+));
+
+//carbon_get_post_meta($id, $name, $type = null):
+
+
